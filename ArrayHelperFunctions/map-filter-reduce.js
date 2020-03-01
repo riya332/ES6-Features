eba99dd34d38ae0fbc5c8134db3908843431276c
Riya Prasad <ripras@microsoft.com>
2020-03-01 13:22:28 +0530

    map-filter-reduce exmple

---
+++ ArrayHelperFunctions/map-filter-reduce.js
@@ -0,0 +1,19 @@
+var companies = ['MICROSOFT', 'APPLE', 'GOOGLE', 'AMAZON'];
+function changeCase(str){
+    return str.toLowerCase();
+}
+ let mappedResult = companies.map(changeCase);
+ console.log('MAP EXAMPLE :' + mappedResult);
+ 
+function filterStr(str){
+    return str.startsWith('A');
+}
+let filteredCompanies = companies.filter(filterStr);
+console.log('FILTER EXAMPLE: ' + filteredCompanies);
+
+
+function concatenateStrings(baseString, str1){
+    return baseString + ', ' + str1;
+}
+let reducedResult = companies.reduce(concatenateStrings, '')
+console.log(reducedResult.toString());
